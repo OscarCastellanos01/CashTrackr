a2f46ff74fe20f7fda9252ac7635f8adb5539fd1
Oscar Castellanos <oscareliascastellanoscordero@gmail.com>
2026-05-24 11:25:07 -0600

    Editar Presupuestos

--- app/Http/Controllers/BudgetController.php
+++ app/Http/Controllers/BudgetController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\BudgetRequest;
 use App\Models\Budget;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Attributes\Controllers\Authorize;
 use Illuminate\Routing\Attributes\Controllers\Middleware;
 use Illuminate\Support\Facades\Auth;
 
@@ -48,7 +49,7 @@ class BudgetController extends Controller
 
         $budget = Auth::user()->budgets()->create($request->validated());
 
-        return redirect()->route('dashboard');
+        return redirect()->route('dashboard')->with('success', 'Presupuesto creado correctamente.');
     }
 
     /**
@@ -62,17 +63,23 @@ class BudgetController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
+    #[Authorize('update', 'budget')]
     public function edit(Budget $budget)
     {
-        //
+        return view('budgets.edit', [
+            'budget' => $budget
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Budget $budget)
+    #[Authorize('update', 'budget')]
+    public function update(BudgetRequest $request, Budget $budget)
     {
-        //
+        $budget->update($request->validated());
+
+        return redirect()->route('dashboard')->with('success', 'Presupuesto actualizado correctamente.');
     }
 
     /**
